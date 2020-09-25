402898bb93faca866623d30e3f80b90e4398dac9
Marcin Kowalczyk <qrczak@google.com>
2020-09-25 06:56:49 +0200

    Support Hadoop Snappy, which is similar to Framed Snappy.
    https://github.com/kubo/snzip#hadoop-snappy-format
    
    PiperOrigin-RevId: 333664112

---
+++ riegeli/bytes/hadoop_snappy_reader.cc
@@ -0,0 +1,143 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "riegeli/bytes/hadoop_snappy_reader.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <cstring>
+#include <limits>
+
+#include "absl/base/optimization.h"
+#include "absl/status/status.h"
+#include "absl/strings/string_view.h"
+#include "riegeli/base/base.h"
+#include "riegeli/base/buffer.h"
+#include "riegeli/base/status.h"
+#include "riegeli/bytes/endian_reading.h"
+#include "riegeli/bytes/pullable_reader.h"
+#include "riegeli/bytes/reader.h"
+#include "snappy.h"
+
+namespace riegeli {
+
+void HadoopSnappyReaderBase::Initialize(Reader* src) {
+  RIEGELI_ASSERT(src != nullptr)
+      << "Failed precondition of HadoopSnappyReader: null Reader pointer";
+  if (ABSL_PREDICT_FALSE(!src->healthy()) && src->available() == 0) {
+    Fail(*src);
+    return;
+  }
+}
+
+void HadoopSnappyReaderBase::Done() {
+  if (ABSL_PREDICT_FALSE(truncated_)) {
+    Reader& src = *src_reader();
+    Fail(Annotate(
+        absl::DataLossError("Truncated HadoopSnappy-compressed stream"),
+        absl::StrCat("at byte ", src.pos())));
+  }
+  PullableReader::Done();
+}
+
+bool HadoopSnappyReaderBase::FailInvalidStream(absl::string_view message) {
+  Reader& src = *src_reader();
+  return Fail(
+      Annotate(absl::DataLossError(absl::StrCat(
+                   "Invalid HadoopSnappy-compressed stream: ", message)),
+               absl::StrCat("at byte ", src.pos())));
+}
+
+bool HadoopSnappyReaderBase::Fail(absl::Status status) {
+  RIEGELI_ASSERT(!status.ok())
+      << "Failed precondition of Object::Fail(): status not failed";
+  return FailWithoutAnnotation(
+      Annotate(status, absl::StrCat("at uncompressed byte ", pos())));
+}
+
+bool HadoopSnappyReaderBase::PullSlow(size_t min_length,
+                                      size_t recommended_length) {
+  RIEGELI_ASSERT_LT(available(), min_length)
+      << "Failed precondition of Reader::PullSlow(): "
+         "enough data available, use Pull() instead";
+  if (ABSL_PREDICT_FALSE(!PullUsingScratch(min_length, recommended_length))) {
+    return available() >= min_length;
+  }
+  if (ABSL_PREDICT_FALSE(!healthy())) return false;
+  Reader& src = *src_reader();
+  truncated_ = false;
+  while (remaining_chunk_length_ == 0) {
+    if (ABSL_PREDICT_FALSE(!src.Pull(sizeof(uint32_t)))) {
+      set_buffer();
+      if (ABSL_PREDICT_FALSE(!src.healthy())) return Fail(src);
+      if (ABSL_PREDICT_FALSE(src.available() > 0)) truncated_ = true;
+      return false;
+    }
+    remaining_chunk_length_ = ReadBigEndian32(src.cursor());
+    src.move_cursor(sizeof(uint32_t));
+  }
+  size_t uncompressed_length;
+  char* uncompressed_data;
+  do {
+    if (ABSL_PREDICT_FALSE(!src.Pull(sizeof(uint32_t)))) {
+      set_buffer();
+      if (ABSL_PREDICT_FALSE(!src.healthy())) return Fail(src);
+      truncated_ = true;
+      return false;
+    }
+    const uint32_t compressed_length = ReadBigEndian32(src.cursor());
+    if (ABSL_PREDICT_FALSE(compressed_length >
+                           std::numeric_limits<uint32_t>::max() -
+                               sizeof(uint32_t))) {
+      set_buffer();
+      return FailInvalidStream("compressed length too large");
+    }
+    if (ABSL_PREDICT_FALSE(!src.Pull(sizeof(uint32_t) + compressed_length))) {
+      set_buffer();
+      if (ABSL_PREDICT_FALSE(!src.healthy())) return Fail(src);
+      truncated_ = true;
+      return false;
+    }
+    const char* const compressed_data = src.cursor() + sizeof(uint32_t);
+    if (ABSL_PREDICT_FALSE(!snappy::GetUncompressedLength(
+            compressed_data, compressed_length, &uncompressed_length))) {
+      set_buffer();
+      return FailInvalidStream("invalid uncompressed length");
+    }
+    if (ABSL_PREDICT_FALSE(uncompressed_length > remaining_chunk_length_)) {
+      set_buffer();
+      return FailInvalidStream("uncompressed length too large");
+    }
+    uncompressed_.Resize(uncompressed_length);
+    uncompressed_data = uncompressed_.GetData();
+    if (ABSL_PREDICT_FALSE(!snappy::RawUncompress(
+            compressed_data, compressed_length, uncompressed_data))) {
+      set_buffer();
+      return FailInvalidStream("invalid compressed data");
+    }
+    src.move_cursor(sizeof(uint32_t) + compressed_length);
+  } while (uncompressed_length == 0);
+  remaining_chunk_length_ -= uncompressed_length;
+  if (ABSL_PREDICT_FALSE(uncompressed_length >
+                         std::numeric_limits<Position>::max() - limit_pos())) {
+    set_buffer(uncompressed_data);
+    return FailOverflow();
+  }
+  set_buffer(uncompressed_data, uncompressed_length);
+  move_limit_pos(available());
+  return true;
+}
+
+}  // namespace riegeli
---
+++ riegeli/bytes/hadoop_snappy_writer.h
@@ -0,0 +1,288 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RIEGELI_BYTES_HADOOP_SNAPPY_WRITER_H_
+#define RIEGELI_BYTES_HADOOP_SNAPPY_WRITER_H_
+
+#include <stddef.h>
+
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+#include "absl/base/optimization.h"
+#include "riegeli/base/base.h"
+#include "riegeli/base/buffer.h"
+#include "riegeli/base/dependency.h"
+#include "riegeli/base/object.h"
+#include "riegeli/base/resetter.h"
+#include "riegeli/bytes/pushable_writer.h"
+#include "riegeli/bytes/writer.h"
+
+namespace riegeli {
+
+// Template parameter independent part of `HadoopSnappyWriter`.
+class HadoopSnappyWriterBase : public PushableWriter {
+ public:
+  class Options {
+   public:
+    Options() noexcept {}
+
+    // Expected uncompressed size, or 0 if unknown. This may improve performance
+    // and memory usage.
+    //
+    // If the size hint turns out to not match reality, nothing breaks.
+    Options& set_size_hint(Position size_hint) & {
+      size_hint_ = size_hint;
+      return *this;
+    }
+    Options&& set_size_hint(Position size_hint) && {
+      return std::move(set_size_hint(size_hint));
+    }
+    Position size_hint() const { return size_hint_; }
+
+   private:
+    Position size_hint_ = 0;
+  };
+
+  // Returns the compressed `Writer`. Unchanged by `Close()`.
+  virtual Writer* dest_writer() = 0;
+  virtual const Writer* dest_writer() const = 0;
+
+  // `HadoopSnappyWriterBase` overrides `Writer::Fail()` to annotate the status
+  // with the current position, clarifying that this is the uncompressed
+  // position. A status propagated from `*dest_writer()` might carry annotation
+  // with the compressed position.
+  using PushableWriter::Fail;
+  ABSL_ATTRIBUTE_COLD bool Fail(absl::Status status) override;
+
+  bool Flush(FlushType flush_type) override;
+
+ protected:
+  HadoopSnappyWriterBase() noexcept : PushableWriter(kInitiallyClosed) {}
+
+  explicit HadoopSnappyWriterBase(Position size_hint);
+
+  HadoopSnappyWriterBase(HadoopSnappyWriterBase&& that) noexcept;
+  HadoopSnappyWriterBase& operator=(HadoopSnappyWriterBase&& that) noexcept;
+
+  void Reset();
+  void Reset(Position size_hint);
+  void Initialize(Writer* dest);
+
+  void Done() override;
+  bool PushSlow(size_t min_length, size_t recommended_length) override;
+
+ private:
+  // Compresses buffered data, but unlike `PushSlow()`, does not ensure that a
+  // buffer is allocated.
+  //
+  // Precondition: `healthy()`
+  //
+  // Postcondition: `written_to_buffer() == 0`
+  bool PushInternal(Writer& dest);
+
+  Position size_hint_ = 0;
+  // Buffered uncompressed data.
+  Buffer uncompressed_;
+
+  // Invariants if scratch is not used:
+  //   `start() == nullptr` or `start() == uncompressed_.GetData()`
+  //   `buffer_size() <= snappy::kBlockSize`
+};
+
+// A `Writer` which compresses data with Hadoop Snappy format before passing it
+// to another `Writer`:
+// https://github.com/kubo/snzip#hadoop-snappy-format
+//
+// The `Dest` template parameter specifies the type of the object providing and
+// possibly owning the compressed `Writer`. `Dest` must support
+// `Dependency<Writer*, Dest>`, e.g. `Writer*` (not owned, default),
+// `std::unique_ptr<Writer>` (owned), `ChainWriter<>` (owned).
+//
+// The compressed `Writer` must not be accessed until the `HadoopSnappyWriter`
+// is closed or no longer used.
+template <typename Dest = Writer*>
+class HadoopSnappyWriter : public HadoopSnappyWriterBase {
+ public:
+  // Creates a closed `HadoopSnappyWriter`.
+  HadoopSnappyWriter() noexcept {}
+
+  // Will write to the compressed `Writer` provided by `dest`.
+  explicit HadoopSnappyWriter(const Dest& dest, Options options = Options());
+  explicit HadoopSnappyWriter(Dest&& dest, Options options = Options());
+
+  // Will write to the compressed `Writer` provided by a `Dest` constructed from
+  // elements of `dest_args`. This avoids constructing a temporary `Dest` and
+  // moving from it.
+  template <typename... DestArgs>
+  explicit HadoopSnappyWriter(std::tuple<DestArgs...> dest_args,
+                              Options options = Options());
+
+  HadoopSnappyWriter(HadoopSnappyWriter&& that) noexcept;
+  HadoopSnappyWriter& operator=(HadoopSnappyWriter&& that) noexcept;
+
+  // Makes `*this` equivalent to a newly constructed `HadoopSnappyWriter`. This
+  // avoids constructing a temporary `HadoopSnappyWriter` and moving from it.
+  void Reset();
+  void Reset(const Dest& dest, Options options = Options());
+  void Reset(Dest&& dest, Options options = Options());
+  template <typename... DestArgs>
+  void Reset(std::tuple<DestArgs...> dest_args, Options options = Options());
+
+  // Returns the object providing and possibly owning the compressed `Writer`.
+  // Unchanged by `Close()`.
+  Dest& dest() { return dest_.manager(); }
+  const Dest& dest() const { return dest_.manager(); }
+  Writer* dest_writer() override { return dest_.get(); }
+  const Writer* dest_writer() const override { return dest_.get(); }
+
+ protected:
+  void Done() override;
+
+ private:
+  // The object providing and possibly owning the compressed `Writer`.
+  Dependency<Writer*, Dest> dest_;
+};
+
+// Support CTAD.
+#if __cpp_deduction_guides
+template <typename Dest>
+HadoopSnappyWriter(Dest&& dest, HadoopSnappyWriterBase::Options options =
+                                    HadoopSnappyWriterBase::Options())
+    -> HadoopSnappyWriter<std::decay_t<Dest>>;
+template <typename... DestArgs>
+HadoopSnappyWriter(
+    std::tuple<DestArgs...> dest_args,
+    HadoopSnappyWriterBase::Options options = HadoopSnappyWriterBase::Options())
+    -> HadoopSnappyWriter<void>;  // Delete.
+#endif
+
+// Implementation details follow.
+
+inline HadoopSnappyWriterBase::HadoopSnappyWriterBase(Position size_hint)
+    : PushableWriter(kInitiallyOpen), size_hint_(size_hint) {}
+
+inline HadoopSnappyWriterBase::HadoopSnappyWriterBase(
+    HadoopSnappyWriterBase&& that) noexcept
+    : PushableWriter(std::move(that)),
+      // Using `that` after it was moved is correct because only the base class
+      // part was moved.
+      size_hint_(that.size_hint_),
+      uncompressed_(std::move(that.uncompressed_)) {}
+
+inline HadoopSnappyWriterBase& HadoopSnappyWriterBase::operator=(
+    HadoopSnappyWriterBase&& that) noexcept {
+  PushableWriter::operator=(std::move(that));
+  // Using `that` after it was moved is correct because only the base class part
+  // was moved.
+  size_hint_ = that.size_hint_;
+  uncompressed_ = std::move(that.uncompressed_);
+  return *this;
+}
+
+inline void HadoopSnappyWriterBase::Reset() {
+  PushableWriter::Reset(kInitiallyClosed);
+  size_hint_ = 0;
+}
+
+inline void HadoopSnappyWriterBase::Reset(Position size_hint) {
+  PushableWriter::Reset(kInitiallyOpen);
+  size_hint_ = SaturatingIntCast<size_t>(size_hint);
+}
+
+template <typename Dest>
+inline HadoopSnappyWriter<Dest>::HadoopSnappyWriter(const Dest& dest,
+                                                    Options options)
+    : HadoopSnappyWriterBase(options.size_hint()), dest_(dest) {
+  Initialize(dest_.get());
+}
+
+template <typename Dest>
+inline HadoopSnappyWriter<Dest>::HadoopSnappyWriter(Dest&& dest,
+                                                    Options options)
+    : HadoopSnappyWriterBase(options.size_hint()), dest_(std::move(dest)) {
+  Initialize(dest_.get());
+}
+
+template <typename Dest>
+template <typename... DestArgs>
+inline HadoopSnappyWriter<Dest>::HadoopSnappyWriter(
+    std::tuple<DestArgs...> dest_args, Options options)
+    : HadoopSnappyWriterBase(options.size_hint()), dest_(std::move(dest_args)) {
+  Initialize(dest_.get());
+}
+
+template <typename Dest>
+inline HadoopSnappyWriter<Dest>::HadoopSnappyWriter(
+    HadoopSnappyWriter&& that) noexcept
+    : HadoopSnappyWriterBase(std::move(that)),
+      // Using `that` after it was moved is correct because only the base class
+      // part was moved.
+      dest_(std::move(that.dest_)) {}
+
+template <typename Dest>
+inline HadoopSnappyWriter<Dest>& HadoopSnappyWriter<Dest>::operator=(
+    HadoopSnappyWriter&& that) noexcept {
+  HadoopSnappyWriterBase::operator=(std::move(that));
+  // Using `that` after it was moved is correct because only the base class part
+  // was moved.
+  dest_ = std::move(that.dest_);
+  return *this;
+}
+
+template <typename Dest>
+inline void HadoopSnappyWriter<Dest>::Reset() {
+  HadoopSnappyWriterBase::Reset();
+  dest_.Reset();
+}
+
+template <typename Dest>
+inline void HadoopSnappyWriter<Dest>::Reset(const Dest& dest, Options options) {
+  HadoopSnappyWriterBase::Reset(options.size_hint());
+  dest_.Reset(dest);
+  Initialize(dest_.get());
+}
+
+template <typename Dest>
+inline void HadoopSnappyWriter<Dest>::Reset(Dest&& dest, Options options) {
+  HadoopSnappyWriterBase::Reset(options.size_hint());
+  dest_.Reset(std::move(dest));
+  Initialize(dest_.get());
+}
+
+template <typename Dest>
+template <typename... DestArgs>
+inline void HadoopSnappyWriter<Dest>::Reset(std::tuple<DestArgs...> dest_args,
+                                            Options options) {
+  HadoopSnappyWriterBase::Reset(options.size_hint());
+  dest_.Reset(std::move(dest_args));
+  Initialize(dest_.get());
+}
+
+template <typename Dest>
+void HadoopSnappyWriter<Dest>::Done() {
+  HadoopSnappyWriterBase::Done();
+  if (dest_.is_owning()) {
+    if (ABSL_PREDICT_FALSE(!dest_->Close())) Fail(*dest_);
+  }
+}
+
+template <typename Dest>
+struct Resetter<HadoopSnappyWriter<Dest>>
+    : ResetterByReset<HadoopSnappyWriter<Dest>> {};
+
+}  // namespace riegeli
+
+#endif  // RIEGELI_BYTES_HADOOP_SNAPPY_WRITER_H_
