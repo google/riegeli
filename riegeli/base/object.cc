c21b744169e1d4f82f2a14386ddd1c67c779462a
Marcin Kowalczyk <qrczak@google.com>
2022-02-16 12:12:21 +0100

    Fix the name in the definition of a static class member, needed before C++17.
    PiperOrigin-RevId: 429001203

--- riegeli/base/object.cc
+++ riegeli/base/object.cc
@@ -28,7 +28,7 @@ namespace riegeli {
 // namespace scope is required. Since C++17 these definitions are deprecated:
 // http://en.cppreference.com/w/cpp/language/static
 #if __cplusplus < 201703
-constexpr uintptr_t ObjectState::kHealthy;
+constexpr uintptr_t ObjectState::kOk;
 constexpr uintptr_t ObjectState::kClosedSuccessfully;
 #endif
 
