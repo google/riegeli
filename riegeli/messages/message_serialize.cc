6c6da2cc2b1dd044af636f6d909d9be538cbabf1
Marcin Kowalczyk <qrczak@google.com>
2022-06-30 15:38:51 +0200

    Optimize serialization of small messages (up to 255 bytes): use `SerializeWithCachedSizesToArray()` if possible.
    Optimize `SerializeTo{String,Chain,Cord}()` for small messages (up to 255
    bytes): avoid creation of the `Writer`, given that making a flat output is
    considered harmless.
    
    For `SerializeToString()` there is no limit of 255 bytes because the result is
    always flat.
    
    PiperOrigin-RevId: 458211680

--- riegeli/messages/message_serialize.cc
+++ riegeli/messages/message_serialize.cc
@@ -19,15 +19,19 @@
 
 #include <limits>
 #include <string>
+#include <utility>
 
 #include "absl/base/attributes.h"
 #include "absl/base/optimization.h"
 #include "absl/status/status.h"
 #include "absl/strings/cord.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/span.h"
 #include "google/protobuf/io/coded_stream.h"
 #include "google/protobuf/message_lite.h"
 #include "riegeli/base/base.h"
+#include "riegeli/base/buffer.h"
 #include "riegeli/base/chain.h"
 #include "riegeli/bytes/chain_writer.h"
 #include "riegeli/bytes/cord_writer.h"
@@ -40,13 +44,18 @@ namespace riegeli {
 namespace {
 
 ABSL_ATTRIBUTE_COLD inline absl::Status FailSizeOverflow(
-    const google::protobuf::MessageLite& src, Writer& dest, size_t size) {
-  return dest.AnnotateStatus(absl::ResourceExhaustedError(
+    const google::protobuf::MessageLite& src, size_t size) {
+  return absl::ResourceExhaustedError(
       absl::StrCat("Failed to serialize message of type ", src.GetTypeName(),
-                   " because its size must be smaller than 2GiB: ", size)));
+                   " because its size must be smaller than 2GiB: ", size));
 }
 
-inline absl::Status SerializeToWriterHavingSize(
+ABSL_ATTRIBUTE_COLD inline absl::Status FailSizeOverflow(
+    const google::protobuf::MessageLite& src, Writer& dest, size_t size) {
+  return dest.AnnotateStatus(FailSizeOverflow(src, size));
+}
+
+inline absl::Status SerializeToWriterUsingStream(
     const google::protobuf::MessageLite& src, Writer& dest, bool deterministic,
     size_t size) {
   WriterOutputStream output_stream(&dest);
@@ -59,7 +68,7 @@ inline absl::Status SerializeToWriterHavingSize(
   RIEGELI_ASSERT(!coded_stream.HadError())
       << "Failed to serialize message of type " << src.GetTypeName()
       << ": SerializeWithCachedSizes() failed for an unknown reason";
-  RIEGELI_ASSERT_EQ(size, src.ByteSizeLong())
+  RIEGELI_ASSERT_EQ(src.ByteSizeLong(), size)
       << src.GetTypeName() << " was modified concurrently during serialization";
   RIEGELI_ASSERT_EQ(IntCast<size_t>(coded_stream.ByteCount()), size)
       << "Byte size calculation and serialization were inconsistent. This "
@@ -67,6 +76,33 @@ inline absl::Status SerializeToWriterHavingSize(
          "concurrent modification of "
       << src.GetTypeName();
   return absl::OkStatus();
+}
+
+inline absl::Status SerializeToWriterHavingSize(
+    const google::protobuf::MessageLite& src, Writer& dest, bool deterministic,
+    size_t size) {
+  if (size <= kMaxBytesToCopy &&
+      deterministic == google::protobuf::io::CodedOutputStream::
+                           IsDefaultSerializationDeterministic()) {
+    // The data are small, so making a flat output is harmless.
+    // `SerializeWithCachedSizesToArray()` is faster than
+    // `SerializeWithCachedSizes()`.
+    if (ABSL_PREDICT_FALSE(!dest.Push(size))) return dest.status();
+    char* const cursor =
+        reinterpret_cast<char*>(src.SerializeWithCachedSizesToArray(
+            reinterpret_cast<uint8_t*>(dest.cursor())));
+    RIEGELI_ASSERT_EQ(src.ByteSizeLong(), size)
+        << src.GetTypeName()
+        << " was modified concurrently during serialization";
+    RIEGELI_ASSERT_EQ(PtrDistance(dest.cursor(), cursor), size)
+        << "Byte size calculation and serialization were inconsistent. This "
+           "may indicate a bug in protocol buffers or it may be caused by "
+           "concurrent modification of "
+        << src.GetTypeName();
+    dest.set_cursor(cursor);
+    return absl::OkStatus();
+  }
+  return SerializeToWriterUsingStream(src, dest, deterministic, size);
 }
 
 }  // namespace
@@ -111,17 +147,131 @@ absl::Status SerializeLengthPrefixedToWriter(
 
 absl::Status SerializeToString(const google::protobuf::MessageLite& src,
                                std::string& dest, SerializeOptions options) {
-  return SerializeToWriter(src, StringWriter<>(&dest), options);
+  RIEGELI_ASSERT(options.partial() || src.IsInitialized())
+      << "Failed to serialize message of type " << src.GetTypeName()
+      << " because it is missing required fields: "
+      << src.InitializationErrorString();
+  const size_t size = options.GetByteSize(src);
+  if (ABSL_PREDICT_FALSE(size >
+                         uint32_t{std::numeric_limits<int32_t>::max()})) {
+    return FailSizeOverflow(src, size);
+  }
+  if (options.deterministic() == google::protobuf::io::CodedOutputStream::
+                                     IsDefaultSerializationDeterministic()) {
+    // Creating a string, which is necessarily flat.
+    // `SerializeWithCachedSizesToArray()` is faster than
+    // `SerializeWithCachedSizes()`.
+    dest.clear();
+    dest.resize(size);
+    char* const cursor =
+        reinterpret_cast<char*>(src.SerializeWithCachedSizesToArray(
+            reinterpret_cast<uint8_t*>(&dest[0])));
+    RIEGELI_ASSERT_EQ(src.ByteSizeLong(), size)
+        << src.GetTypeName()
+        << " was modified concurrently during serialization";
+    RIEGELI_ASSERT_EQ(PtrDistance(dest.data(), cursor), size)
+        << "Byte size calculation and serialization were inconsistent. This "
+           "may indicate a bug in protocol buffers or it may be caused by "
+           "concurrent modification of "
+        << src.GetTypeName();
+    return absl::OkStatus();
+  }
+  riegeli::StringWriter<> writer(&dest);
+  writer.SetWriteSizeHint(size);
+  const absl::Status status =
+      SerializeToWriterUsingStream(src, writer, options.deterministic(), size);
+  if (!writer.Close()) {
+    RIEGELI_ASSERT_UNREACHABLE()
+        << "A StringWriter has no reason to fail: " << writer.status();
+  }
+  return status;
 }
 
 absl::Status SerializeToChain(const google::protobuf::MessageLite& src,
                               Chain& dest, SerializeOptions options) {
-  return SerializeToWriter(src, ChainWriter<>(&dest), options);
+  RIEGELI_ASSERT(options.partial() || src.IsInitialized())
+      << "Failed to serialize message of type " << src.GetTypeName()
+      << " because it is missing required fields: "
+      << src.InitializationErrorString();
+  const size_t size = options.GetByteSize(src);
+  if (ABSL_PREDICT_FALSE(size >
+                         uint32_t{std::numeric_limits<int32_t>::max()})) {
+    return FailSizeOverflow(src, size);
+  }
+  if (size <= kMaxBytesToCopy &&
+      options.deterministic() == google::protobuf::io::CodedOutputStream::
+                                     IsDefaultSerializationDeterministic()) {
+    // The data are small, so making a flat output is harmless.
+    // `SerializeWithCachedSizesToArray()` is faster than
+    // `SerializeWithCachedSizes()`.
+    dest.Clear();
+    const absl::Span<char> buffer =
+        dest.AppendFixedBuffer(size, Chain::Options().set_size_hint(size));
+    char* const cursor =
+        reinterpret_cast<char*>(src.SerializeWithCachedSizesToArray(
+            reinterpret_cast<uint8_t*>(buffer.data())));
+    RIEGELI_ASSERT_EQ(src.ByteSizeLong(), size)
+        << src.GetTypeName()
+        << " was modified concurrently during serialization";
+    RIEGELI_ASSERT_EQ(PtrDistance(buffer.data(), cursor), size)
+        << "Byte size calculation and serialization were inconsistent. This "
+           "may indicate a bug in protocol buffers or it may be caused by "
+           "concurrent modification of "
+        << src.GetTypeName();
+    return absl::OkStatus();
+  }
+  riegeli::ChainWriter<> writer(&dest);
+  writer.SetWriteSizeHint(size);
+  const absl::Status status =
+      SerializeToWriterUsingStream(src, writer, options.deterministic(), size);
+  if (!writer.Close()) {
+    RIEGELI_ASSERT_UNREACHABLE()
+        << "A ChainWriter has no reason to fail: " << writer.status();
+  }
+  return status;
 }
 
 absl::Status SerializeToCord(const google::protobuf::MessageLite& src,
                              absl::Cord& dest, SerializeOptions options) {
-  return SerializeToWriter(src, CordWriter<>(&dest), options);
+  RIEGELI_ASSERT(options.partial() || src.IsInitialized())
+      << "Failed to serialize message of type " << src.GetTypeName()
+      << " because it is missing required fields: "
+      << src.InitializationErrorString();
+  const size_t size = options.GetByteSize(src);
+  if (ABSL_PREDICT_FALSE(size >
+                         uint32_t{std::numeric_limits<int32_t>::max()})) {
+    return FailSizeOverflow(src, size);
+  }
+  if (size <= kMaxBytesToCopy &&
+      options.deterministic() == google::protobuf::io::CodedOutputStream::
+                                     IsDefaultSerializationDeterministic()) {
+    // The data are small, so making a flat output is harmless.
+    // `SerializeWithCachedSizesToArray()` is faster than
+    // `SerializeWithCachedSizes()`.
+    Buffer buffer(size);
+    char* const cursor =
+        reinterpret_cast<char*>(src.SerializeWithCachedSizesToArray(
+            reinterpret_cast<uint8_t*>(buffer.data())));
+    RIEGELI_ASSERT_EQ(src.ByteSizeLong(), size)
+        << src.GetTypeName()
+        << " was modified concurrently during serialization";
+    RIEGELI_ASSERT_EQ(PtrDistance(buffer.data(), cursor), size)
+        << "Byte size calculation and serialization were inconsistent. This "
+           "may indicate a bug in protocol buffers or it may be caused by "
+           "concurrent modification of "
+        << src.GetTypeName();
+    dest = std::move(buffer).ToCord(absl::string_view(buffer.data(), size));
+    return absl::OkStatus();
+  }
+  riegeli::CordWriter<> writer(&dest);
+  writer.SetWriteSizeHint(size);
+  const absl::Status status =
+      SerializeToWriterUsingStream(src, writer, options.deterministic(), size);
+  if (!writer.Close()) {
+    RIEGELI_ASSERT_UNREACHABLE()
+        << "A CordWriter has no reason to fail: " << writer.status();
+  }
+  return status;
 }
 
 bool WriterOutputStream::Next(void** data, int* size) {
