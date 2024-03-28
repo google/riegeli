019ebe7b4f99a9677bc8f7c4558bf93f58afbd5d
Marcin Kowalczyk <qrczak@google.com>
2024-03-28 15:57:31 +0100

    Fix `riegeli::tensorflow::FileWriter::Reset(kClosed)`: do not call `Initialize()` when `dest_` is empty.
    PiperOrigin-RevId: 619931141

--- riegeli/tensorflow/io/file_writer.h
+++ riegeli/tensorflow/io/file_writer.h
@@ -329,7 +329,6 @@ template <typename Dest>
 inline void FileWriter<Dest>::Reset(Closed) {
   FileWriterBase::Reset(kClosed);
   dest_.Reset();
-  Initialize(dest_.get());
 }
 
 template <typename Dest>
