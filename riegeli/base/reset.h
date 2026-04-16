eb29aa6a92ff38818873c89f08c704037f6a4bf2
Marcin Kowalczyk <qrczak@google.com>
2026-04-16 11:45:02 +0200

    Make `riegeli::Reset(dest, args...)` an effective optimization of `dest = T(args...)` for more types and patterns:
    * `riegeli::Reset(dest, std::in_place, args...)` → `dest.emplace(args...)`
    * `riegeli::Reset(dest, std::in_place_type<Type>, args...)` →
      `dest.emplace<Type>(args...)`
    * `riegeli::Reset(dest, std::in_place_index<index>, args...)` →
      `dest.emplace<index>(args...)`
    * `riegeli::Reset(dest)` → `dest.clear()` (`begin()` and `end()` must be
      defined because this pattern would be risky for non-containers)
    * `riegeli::Reset(dest, args...)` → `dest.assign(args...)`
      (`begin()` and `end()` must be defined)
    
    This covers in particular `std::optional`, `std::variant`, `std::any`,
    `absl::InlinedVector`...
    
    Describe some caveats when resetting may differ from assignment from a newly
    constructed object.
    
    Remove overrides of `RiegeliReset()` which became redundant.
    
    PiperOrigin-RevId: 900610370

--- riegeli/base/reset.h
+++ riegeli/base/reset.h
@@ -17,14 +17,11 @@
 
 #include <stddef.h>
 
-#include <string>
 #include <type_traits>
 #include <utility>
 
 #include "absl/base/nullability.h"
 #include "absl/strings/cord.h"
-#include "absl/strings/string_view.h"
-#include "riegeli/base/assert.h"
 
 ABSL_POINTERS_DEFAULT_NONNULL
 
@@ -36,82 +33,169 @@ namespace riegeli {
 // copy and move constructors which are mirrored by the assignment operators).
 // This avoids constructing a temporary `T` and moving from it.
 //
-// If it is not known whether the given class provides member functions
-// `Reset()`, generic code can use `riegeli::Reset(dest, args...)`.
-// This calls the first defined form among the following:
+// Other conventions exist as well. Generic `riegeli::Reset(dest, args...)`
+// employs them by calling the first defined form among the following:
 //  * `RiegeliReset(dest, args...)`
 //  * `dest.Reset(args...)`
-//  * `dest = args...` (if `args...` has a single element)
+//  * `dest.emplace(args...)` - if `args...` are preceded by `std::in_place`
+//  * `dest.emplace<Type>(args...)` - if `args...` are preceded by
+//     `std::in_place_type<Type>`
+//  * `dest.emplace<index>(args...)` - if `args...` are preceded by
+//     `std::in_place_index<index>`
+//  * `dest.clear()` - if `args...` are empty; `begin()` and `end()` must be
+//     defined because this pattern would be risky for non-containers
+//  * `dest.assign(args...)` - `begin()` and `end()` must be defined
+//  * `dest = args...` - if `args...` has a single element
 //  * `dest = T(args...)`
 //
-// Hence to customize `riegeli::Reset()` for a class `T`, define overloads of
-// either a member function `void T::Reset(...)`, or a free function
+// Hence to customize `riegeli::Reset()` for a class `T`, define overloads
+// of either a member function `void T::Reset(...)`, or a free function
 // `friend void RiegeliReset(T& dest, ...)` as a friend of `T` inside class
 // definition or in the same namespace as `T`, so that it can be found via ADL.
 //
-// `RiegeliReset()` is predefined for `std::string` and `absl::Cord`.
-
-inline void RiegeliReset(std::string& dest) { dest.clear(); }
-
-inline void RiegeliReset(std::string& dest, size_t size, char fill) {
-  dest.assign(size, fill);
-}
-
-inline void RiegeliReset(std::string& dest, absl::string_view src) {
-  dest.assign(src);
-}
-
-inline void RiegeliReset(std::string& dest, const char* src) {
-  dest.assign(src);
-}
-
-inline void RiegeliReset(std::string& dest, const char* absl_nullable src,
-                         size_t length) {
-  RIEGELI_ASSERT(src != nullptr || length == 0)
-      << "Failed precondition of RiegeliReset(): non-empty span from nullptr";
-  dest.assign(src, length);
-}
+// Resetting is not necessarily fully equivalent to assigning from a newly
+// constructed object. They can differ in:
+//  * Assigning from objects previously owned by `dest` (unsafe for resetting).
+//  * Exception safety (resetting is not transactional).
+//  * Reusing existing memory allocations (common for resetting).
+//  * Non-salient attributes like `capacity()`.
 
 inline void RiegeliReset(absl::Cord& dest) { dest.Clear(); }
 
-inline void RiegeliReset(absl::Cord& dest, absl::string_view src) {
-  dest = src;
-}
-
 namespace reset_internal {
 
-template <typename Enable, typename T, typename... Args>
+template <typename T, typename Enable, typename... Args>
 struct HasRiegeliResetImpl : std::false_type {};
 
 template <typename T, typename... Args>
-struct HasRiegeliResetImpl<std::void_t<decltype(RiegeliReset(
+struct HasRiegeliResetImpl<T,
+                           std::void_t<decltype(RiegeliReset(
                                std::declval<T&>(), std::declval<Args>()...))>,
-                           T, Args...> : std::true_type {};
+                           Args...> : std::true_type {};
 
 template <typename T, typename... Args>
-struct HasRiegeliReset : HasRiegeliResetImpl<void, T, Args...> {};
+struct HasRiegeliReset : HasRiegeliResetImpl<T, void, Args...> {};
 
-template <typename Enable, typename T, typename... Args>
+template <typename T, typename Enable, typename... Args>
 struct HasResetImpl : std::false_type {};
 
 template <typename T, typename... Args>
 struct HasResetImpl<
-    std::void_t<decltype(std::declval<T&>().Reset(std::declval<Args>()...))>, T,
+    T, std::void_t<decltype(std::declval<T&>().Reset(std::declval<Args>()...))>,
     Args...> : std::true_type {};
 
 template <typename T, typename... Args>
-struct HasReset : HasResetImpl<void, T, Args...> {};
+struct HasReset : HasResetImpl<T, void, Args...> {};
 
-template <typename Enable, typename T, typename... Args>
+template <typename T, typename Enable, typename... Args>
+struct HasEmplaceImpl : std::false_type {};
+
+template <typename T, typename... Args>
+struct HasEmplaceImpl<
+    T,
+    std::void_t<decltype(std::declval<T&>().emplace(std::declval<Args>()...))>,
+    std::in_place_t, Args...> : std::true_type {};
+
+template <typename T, typename... Args>
+struct HasEmplaceImpl<
+    T,
+    std::void_t<decltype(std::declval<T&>().emplace(std::declval<Args>()...))>,
+    const std::in_place_t&, Args...> : std::true_type {};
+
+template <typename T, typename... Args>
+struct HasEmplace : HasEmplaceImpl<T, void, Args...> {};
+
+template <typename T, typename Enable, typename... Args>
+struct HasEmplaceWithTypeImpl : std::false_type {};
+
+template <typename T, typename Type, typename... Args>
+struct HasEmplaceWithTypeImpl<
+    T,
+    std::void_t<decltype(std::declval<T&>().template emplace<Type>(
+        std::declval<Args>()...))>,
+    std::in_place_type_t<Type>, Args...> : std::true_type {};
+
+template <typename T, typename Type, typename... Args>
+struct HasEmplaceWithTypeImpl<
+    T,
+    std::void_t<decltype(std::declval<T&>().template emplace<Type>(
+        std::declval<Args>()...))>,
+    const std::in_place_type_t<Type>&, Args...> : std::true_type {};
+
+template <typename T, typename... Args>
+struct HasEmplaceWithType : HasEmplaceWithTypeImpl<T, void, Args...> {};
+
+template <typename T, typename Enable, typename... Args>
+struct HasEmplaceWithIndexImpl : std::false_type {};
+
+template <typename T, size_t index, typename... Args>
+struct HasEmplaceWithIndexImpl<
+    T,
+    std::void_t<decltype(std::declval<T&>().template emplace<index>(
+        std::declval<Args>()...))>,
+    std::in_place_index_t<index>, Args...> : std::true_type {};
+
+template <typename T, size_t index, typename... Args>
+struct HasEmplaceWithIndexImpl<
+    T,
+    std::void_t<decltype(std::declval<T&>().template emplace<index>(
+        std::declval<Args>()...))>,
+    const std::in_place_index_t<index>&, Args...> : std::true_type {};
+
+template <typename T, typename... Args>
+struct HasEmplaceWithIndex : HasEmplaceWithIndexImpl<T, void, Args...> {};
+
+template <typename T, typename Enable, typename... Args>
+struct HasClearImpl : std::false_type {};
+
+template <typename T>
+struct HasClearImpl<T, std::void_t<decltype(std::declval<T&>().clear()),
+                                   decltype(std::declval<T&>().begin()),
+                                   decltype(std::declval<T&>().end())>>
+    : std::true_type {};
+
+template <typename T, typename... Args>
+struct HasClear : HasClearImpl<T, void, Args...> {};
+
+template <typename T, typename Enable, typename... Args>
+struct HasAssignImpl : std::false_type {};
+
+template <typename T, typename... Args>
+struct HasAssignImpl<
+    T,
+    std::void_t<decltype(std::declval<T&>().assign(std::declval<Args>()...)),
+                decltype(std::declval<T&>().begin()),
+                decltype(std::declval<T&>().end())>,
+    Args...> : std::true_type {};
+
+template <typename T, typename... Args>
+struct HasAssign : HasAssignImpl<T, void, Args...> {};
+
+template <typename T, typename Enable, typename... Args>
 struct HasAssignmentImpl : std::false_type {};
 
 template <typename T, typename Arg>
 struct HasAssignmentImpl<
-    std::void_t<decltype(std::declval<T&>() = std::declval<Arg>())>, T, Arg>
+    T, std::void_t<decltype(std::declval<T&>() = std::declval<Arg>())>, Arg>
     : std::true_type {};
 
 template <typename T, typename... Args>
-struct HasAssignment : HasAssignmentImpl<void, T, Args...> {};
+struct HasAssignment : HasAssignmentImpl<T, void, Args...> {};
+
+template <typename T, typename... Args>
+inline void CallEmplace(T& dest, std::in_place_t, Args&&... args) {
+  dest.emplace(std::forward<Args>(args)...);
+}
+
+template <typename T, typename Type, typename... Args>
+inline void CallEmplace(T& dest, std::in_place_type_t<Type>, Args&&... args) {
+  dest.template emplace<Type>(std::forward<Args>(args)...);
+}
+
+template <typename T, size_t index, typename... Args>
+inline void CallEmplace(T& dest, std::in_place_index_t<index>, Args&&... args) {
+  dest.template emplace<index>(std::forward<Args>(args)...);
+}
 
 }  // namespace reset_internal
 
@@ -121,6 +205,11 @@ template <typename T, typename... Args>
 struct SupportsReset
     : std::disjunction<reset_internal::HasRiegeliReset<T, Args...>,
                        reset_internal::HasReset<T, Args...>,
+                       reset_internal::HasEmplace<T, Args...>,
+                       reset_internal::HasEmplaceWithType<T, Args...>,
+                       reset_internal::HasEmplaceWithIndex<T, Args...>,
+                       reset_internal::HasClear<T, Args...>,
+                       reset_internal::HasAssign<T, Args...>,
                        reset_internal::HasAssignment<T, Args...>,
                        std::conjunction<std::is_constructible<T, Args...>,
                                         std::is_move_assignable<T>>> {};
@@ -132,6 +221,16 @@ inline void Reset(T& dest, Args&&... args) {
     RiegeliReset(dest, std::forward<Args>(args)...);
   } else if constexpr (reset_internal::HasReset<T, Args...>::value) {
     dest.Reset(std::forward<Args>(args)...);
+  } else if constexpr (std::disjunction_v<
+                           reset_internal::HasEmplace<T, Args...>,
+                           reset_internal::HasEmplaceWithType<T, Args...>,
+                           reset_internal::HasEmplaceWithIndex<T, Args...>>) {
+    reset_internal::CallEmplace(dest, std::forward<Args>(args)...);
+  } else if constexpr (reset_internal::HasClear<T, Args...>::value) {
+    static_assert(sizeof...(Args) == 0, "Implied by HasClear");
+    dest.clear();
+  } else if constexpr (reset_internal::HasAssign<T, Args...>::value) {
+    dest.assign(std::forward<Args>(args)...);
   } else if constexpr (reset_internal::HasAssignment<T, Args...>::value) {
     static_assert(sizeof...(Args) == 1, "Implied by HasAssignment");
     dest = std::forward<Args...>(args...);
@@ -139,8 +238,7 @@ inline void Reset(T& dest, Args&&... args) {
                                           std::is_move_assignable<T>>) {
     dest = T(std::forward<Args>(args)...);
   } else {
-    static_assert(std::is_void_v<std::void_t<T>>,
-                  "Parameters excluded by function signature");
+    static_assert(std::is_void_v<std::void_t<T>>, "Implied by SupportsReset");
   }
 }
 
